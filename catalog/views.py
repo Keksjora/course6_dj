e663fd6f136cec4b2ed1c7c32d9ca23e070b4c4c
Max Odegov <zoomzooom228@mail.ru>
2024-09-21 22:40:47 +0700

    вывод 5 продуктов

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Product, Category
 
 
 def home(request):
-    return render(request, 'home.html')
+
+    latest_products = Product.objects.order_by('created_at')[:5]
+
+    for product in latest_products:
+        print(f"Product: {product.name}, Price: {product.price}")
+    return render(request, 'catalog/home.html')
 
 
 def contacts(request):
@@ -12,4 +18,4 @@ def contacts(request):
         phone = request.POST.get("phone")
         message = request.POST.get("message")
         return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
