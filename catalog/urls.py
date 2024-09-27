999da121b0495566fdddeac6e3c02455560953ca
Max Odegov <zoomzooom228@mail.ru>
2024-09-27 19:21:29 +0700

    fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,6 +9,8 @@ app_name = "catalog"
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
+    path("product/<int:product_id>/", views.product_info, name="product_info"),
+    path("add_product/", views.add_product, name="add_product"),
 ]
 
 if settings.DEBUG:
